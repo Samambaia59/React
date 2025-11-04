3b27dcc2e48ba2458e7dd03478c2e29a79ea3f3e
Samambaia59 <gunevesgustavo@gmail.com>
2025-11-04 10:34:40 -0300

    1-c)

---
+++ 1-c).jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import './App.css';
+
+ function DataHoraAtual(){
+  const agora = new Date()
+  const diasSemana = ['Domingo','Segunda-Feira','Terça-Feira','Quarta-Feira','Quinta-Feira','Sexta-Feira','Sábado'];
+  const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+}
