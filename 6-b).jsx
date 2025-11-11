31ab2c2e07dbbe0fa3da263682bc52147e22de11
Samambaia59 <gunevesgustavo@gmail.com>
2025-11-11 09:53:27 -0300

    6-b)

---
+++ 6-b).jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function MostrarEsconder(){
+  const[mostrar, setMostrar] = useState(false);
+ 
+  return(
+  <div>
+    <button onClick={() => setMostrar (!mostrar)}>
+      {mostrar ? 'Esconder foto' : 'Mostrar foto'}
+    </button>
+    {mostrar && <img src="src/assets/GatoDandoLingua.jpg"/>}
+  </div>
+
+
+  );
+}
+
+
+export default MostrarEsconder
