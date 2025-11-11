94d343ce056800acd120e4651ff3d92ad1cdd62e
Samambaia59 <gunevesgustavo@gmail.com>
2025-11-11 08:47:47 -0300

    3-a)

---
+++ 3-a).jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function ListaHobbies() {
+  const hobbies = ['Leitura', 'Desenhar', 'Programação', 'Culinária']
+
+
+  return(
+    <div>
+      <h2>Meus Hobbies Favoritos</h2>
+      <ul>
+        {hobbies.map((hobby, index) => (
+         <li key = {index}>{hobby}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
+export default ListaHobbies
