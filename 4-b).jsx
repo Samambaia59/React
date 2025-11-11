a9475c7e4558afffdb2bf654850c22c197200c06
Samambaia59 <gunevesgustavo@gmail.com>
2025-11-11 09:33:32 -0300

    4-b)

---
+++ 4-b).jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function CartaoLivro({titulo, autor, ano, genero}){  
+  return(
+    <div>
+      <h2>{titulo}</h2>
+      <p><strong>Autor: </strong>{autor}</p>
+      <p><strong>Ano: </strong>{ano}</p>
+      <p><strong>Gênero: </strong>{genero}</p>
+    </div>
+  );
+}
+
+
+function App() {
+  return (
+    <div>
+      <CartaoLivro titulo = "O livro do Bill" autor = "Alex Hirsch" ano = {2024} genero = "humor,fantasia sombria,fantasia paranormal"/>
+    </div>
+  );
+}
+
+
+export default App;
