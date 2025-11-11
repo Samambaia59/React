52aec8a471d94a93c242d64c4c04d4d927a2941a
Samambaia59 <gunevesgustavo@gmail.com>
2025-11-11 10:41:55 -0300

    12-b)

---
+++ 12-b).jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [favoritos, setFavoritos] = useState(0);
+  const [favoritado, setFavoritado] = useState(false);
+  const favorito = () => {
+    if (favoritado) {
+      setFavoritos(favoritos - 1);
+      setFavoritado(false);
+    } else {
+      setFavoritos(favoritos + 1);
+      setFavoritado(true);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <h2>Sistema de Favoritos</h2>
+        <button onClick={favorito}>
+          {favoritado ? "Desfavoritar ★" : "Favoritar ☆"}
+        </button>
+        <p>
+          {favoritos} {favoritos === 1 ? "favorito" : "favoritos"}
+        </p>
+      </div>
+    </div>
+  );
+}
+export default App;
